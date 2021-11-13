5632fa3e9bc017d8f8f96ee0f40cc867cc8a0412
tjdans345 <tjdans345@naver.com>
2021-11-13 21:07:24 +0900

    frontMVC refactoring

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,28 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+        return mv;
+    }
+}
